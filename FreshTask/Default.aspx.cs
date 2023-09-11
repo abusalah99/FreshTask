456bf8f59e4d20540ff1f192a887fe554d6fa69f
abu salah <abu_salah@yahoo.com>
2023-09-11 21:23:56 +0300

    Commit

--- FreshTask/Default.aspx.cs
+++ FreshTask/Default.aspx.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 
 namespace FreshTask
 {
@@ -11,35 +12,40 @@ namespace FreshTask
         protected async void Page_Load(object sender, EventArgs e)
         {
             if (IsPostBack)
+                await HandelFormSubmit();
+        }
+
+        private async Task HandelFormSubmit()
+        {
+            if (Request.Form[$"net-value"] != string.Empty)
             {
                 List<Item> items = new List<Item>();
+                List<string> keys = Request.Form.AllKeys.Where(key => key.Contains("Quantity")).ToList();
 
-                if (Request.Form[$"net-value"] != string.Empty)
+                foreach (string key in keys)
                 {
-                    List<string> keys = Request.Form.AllKeys.Where(key => key.Contains("Quantity")).ToList();
-
-                    foreach (string key in keys)
+                    string rowIndex = key.Replace("Quantity", "");
+                    Item item = new Item()
                     {
-                        string rowIndex = key.Replace("Quantity", "");
-                        Item item = new Item()
-                        {
-                            Id = Guid.NewGuid(),
-                            Name = $"Item {rowIndex}",
-                            Quntity = Request.Form[key],
-                            UnitPrice = Request.Form[$"UnitPrice{rowIndex}"]
-                        };
-                        items.Add(item);
-                    }
-
-                    Invoice invoice = new Invoice()
-                    {
-                        Items = items,
-                        Total = Request.Form[$"net-value"]
+                        Id = Guid.NewGuid(),
+                        Name = $"Item {rowIndex}",
+                        Quntity = Request.Form[key],
+                        UnitPrice = Request.Form[$"UnitPrice{rowIndex}"]
                     };
 
-                    await _unitOfWork.Create(invoice);
+                    items.Add(item);
                 }
+
+                Invoice invoice = new Invoice()
+                {
+                    Items = items,
+                    Total = Request.Form[$"net-value"]
+                };
+
+                await SaveBillResultInDb(invoice);
             }
         }
+
+        private async Task SaveBillResultInDb(Invoice invoice) => await _unitOfWork.Create(invoice);
     }
 }
